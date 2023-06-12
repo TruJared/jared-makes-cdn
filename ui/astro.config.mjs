b106193444a946aaafb539c763fc11eca739e9b9
Jared Truscott <jtruscott78@gmail.com>
2023-06-12 04:39:39 -0400

    new design test

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
@@ -9,5 +8,5 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap(), prefetch(), robotsTxt(), image({serviceEntryPoint: '@astrojs/image/sharp'})]
+  integrations: [sitemap(), prefetch(), robotsTxt(), image({serviceEntryPoint: '@astrojs/image/sharp'})]
 });
